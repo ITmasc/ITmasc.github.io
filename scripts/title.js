c421d36a23d1af9b00366a3ca177ce9d07c8f7f2
Mauro <mauroscianca98@gmail.com>
2017-09-30 13:18:33 +0200

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -1,5 +1,5 @@
 var curr_link = window.location.href;
-document.title = curr_link;
+alert(curr_link);
 if (curr_link == "https://itmasc.github.io/"){
   document.title = "ITmasc";
 } else if (curr_link == "https://itmasc.github.io/andr_proj/home.html"){
