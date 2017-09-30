9355f4022621af2bb467c863bf102a8c7fe67ac5
Mauro <mauroscianca98@gmail.com>
2017-09-30 13:12:15 +0200

    Update title.js

--- scripts/title.js
+++ scripts/title.js
@@ -2,9 +2,9 @@ var curr_link = window.location.href;
 
 if (curr_link == "https://itmasc.github.io/"){
   document.title = "ITmasc";
-  $('meta[name="description"]').attr("content", "ITmasc");
 } else if (curr_link == "https://itmasc.github.io/andr_proj/home.html"){
   document.title = "ITmasc - Android";
-  $('meta[name="description"]').attr("content", "ITmasc - Android");
+} else {
+  document.title = "LOL";
 }
 
